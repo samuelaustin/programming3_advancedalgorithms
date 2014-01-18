8f88fa1234b5e999353900488a405b9e45b1eec0
Samuel Austin <samuelaustin_3@hotmail.com>
2014-01-19 00:57:34 +0100

    Started programming 3 assignment

---
+++ src/Main.cpp
@@ -0,0 +1,14 @@
+#include <lemon/list_graph.h>
+
+using namespace lemon;
+
+int main()
+{
+	std::cout << "Lemons" << "\n";
+	
+	ListDigraph g;
+	
+	ListDigraph::Node u = g.addNode();
+	
+	return 0;
+}
