8972e5fd43744b655b7cd6d321cd18832b06e48e
John doh <tavanderlaan@tud203261.tudelft.net>
2014-01-31 18:13:24 +0100

    blaaaaaaaaa

--- graph_generator/main.py
+++ graph_generator/main.py
@@ -2,8 +2,10 @@ import sys
 sys.path.append('./site-packages')
 import networkx as nx
 import numpy as np
+import utils
+from subprocess import call
 
-G=nx.lollipop_graph(10,20)
-for line in nx.generate_edgelist(G,data=False):
-	print(line)
 
+G = nx.lollipop_graph(10,20)
+calout = call(["../build/programming3", utils.dmax(G)])
+print(calout)
