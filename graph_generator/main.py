951c723ae9ac4e8c0ef88073fb70dd14b899fd80
John doh <tavanderlaan@tud203261.tudelft.net>
2014-01-31 18:33:56 +0100

    bla

--- graph_generator/main.py
+++ graph_generator/main.py
@@ -2,10 +2,18 @@ import sys
 sys.path.append('./site-packages')
 import networkx as nx
 import numpy as np
+import pylab as pl
 import utils
-from subprocess import call
 
+import os
+from subprocess import Popen, PIPE
 
-G = nx.lollipop_graph(10,20)
-calout = call(["../build/programming3", utils.dmax(G)])
-print(calout)
+def generate_data():
+	approx_vertex_cover = np.zeros([1,10])
+	for i in range(0,10):
+		G=nx.erdos_renyi_graph(100,0.1*i)
+		process = Popen(["../build/programming3", utils.dmax(G)], stdout=PIPE)	
+		(output, err) = process.communicate()
+		exit_code = process.wait()
+		output
+	plot()
