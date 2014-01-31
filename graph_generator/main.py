70a5370649eaaa6165e0b8849c3424112cb0ec0f
Samuel Austin <samuelaustin_3@hotmail.com>
2014-01-31 20:10:49 +0100

    sdv

--- graph_generator/main.py
+++ graph_generator/main.py
@@ -2,18 +2,39 @@ import sys
 sys.path.append('./site-packages')
 import networkx as nx
 import numpy as np
-import pylab as pl
+#import pylab as pl
+import matplotlib.pyplot as pl
 import utils
 
 import os
 from subprocess import Popen, PIPE
 
 def generate_data():
-	approx_vertex_cover = np.zeros([1,10])
+	approx_vertex_cover = np.zeros([1,11])
+	ind = np.zeros([1,11])
 	for i in range(0,10):
-		G=nx.erdos_renyi_graph(100,0.1*i)
+		G=nx.erdos_renyi_graph(100,0.01*i)
 		process = Popen(["../build/programming3", utils.dmax(G)], stdout=PIPE)	
 		(output, err) = process.communicate()
 		exit_code = process.wait()
-		output
-	plot()
+		data = [int(s) for s in output.split() if s.isdigit()]
+		approx_vertex_cover[0,i]=data[0]
+		ind[0,i]= i
+		nx.draw(G)
+		pl.savefig('g'+str(i)+'.png')
+		pl.clf()
+	return (ind,approx_vertex_cover)
+	
+(ind,data) = generate_data()
+print(data)
+print(ind)
+
+#pl.figure(figsize=(10,6), dpi=80)
+pl.plot(ind, data, linewidth=1.0, linestyle='r--', marker='o')
+#pl.savefig('plot.png', dpi=80)
+pl.show()
+	
+	
+	
+	
+	
